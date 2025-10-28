ad0f5f8fd2a71ddf36cab849366da164caaaae6f
Gunjan Kadam <110162991+Gunjankadam@users.noreply.github.com>
2025-10-28 18:00:30 +0530

    Update User.ts

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,6 +1,6 @@
 import mongoose, { Document, Schema } from "mongoose";
 
-interface UserDocument extends Document {
+export interface UserDocument extends Document {
   email: string;
   password?: string;
   createdAt: Date;
